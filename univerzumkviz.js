f4e89c3e70aae7d33db0c5caac52b40faab3e75b
Berta György <111363208+bertagyorgy@users.noreply.github.com>
2026-01-29 08:50:29 +0100

    Add ertekel function to evaluate quiz answers

---
+++ univerzumkviz.js
@@ -0,0 +1,20 @@
+function ertekel()
+{
+   jo = 0
+   if( document.teszt.k1[1].checked )   jo++
+   if( document.teszt.k2[3].checked )   jo++
+   if( document.teszt.k3[1].checked )   jo++
+   if( document.teszt.k4[2].checked )   jo++
+   if( document.teszt.k5[1].checked )   jo++
+   if( document.teszt.k6[3].checked )   jo++
+   if( document.teszt.k7[0].checked )   jo++
+   if( document.teszt.k8[2].checked )   jo++
+   if( document.teszt.k9[1].checked )   jo++
+   if( document.teszt.k10[1].checked )   jo++
+   if( document.teszt.k11[2].checked )   jo++
+   if( document.teszt.k12[2].checked )   jo++
+   if( document.teszt.k13[1].checked )   jo++
+   if( document.teszt.k14[0].checked )   jo++
+   if( document.teszt.k15[3].checked )   jo++
+   alert( 'Helyes válaszaid száma: ' + jo )
+}
